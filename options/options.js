b19fe4a533f57114c3369e855b7a641fbf45df7a
Crowdbotics <engineering@crowdbotics.com>
2023-06-24 23:09:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-violet-dev-94413",
+  url: "https://super-violet-dev-94413.botics.co",
+  api: "https://super-violet-dev-94413.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
